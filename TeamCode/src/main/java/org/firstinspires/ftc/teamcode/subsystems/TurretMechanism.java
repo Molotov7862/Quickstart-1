47be27a9c38b7353b0119ba16101d6af55f8bd32
Molotov <irfaankhan2008@gmail.com>
2026-02-21 03:30:51 -0500

    Update TurretMechanism.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/TurretMechanism.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/TurretMechanism.java
@@ -1,60 +1,57 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-@Configurable
+
 public class TurretMechanism {
 
     private DcMotorEx turret;
     private Limelight3A limelight;
 
-    public static double kP = 0.0343;
-    public static double kD = 0.00001;
+
+    // remember to tune all of this :)
+    public static double kP = 0.0001;
+    public static double kD = 0.0001;
 
     private double lastError = 0;
-    private double angleTolerance = 0.2;
-    private final double MAX_POWER = 0.8;
+    private double angleTolerance = 1;
+    private final double MAX_POWER = 1.0;
 
     private double power = 0;
 
     private final ElapsedTime timer = new ElapsedTime();
 
 
-    public static int leftLimit  = -600;
-    public static int rightLimit =  600;
+    private final int LEFT_LIMIT = -600;
+    private final int RIGHT_LIMIT = 600;
 
 
     public void init(HardwareMap hwMap, Telemetry telemetry) {
-        limelight.start();
 
         turret = hwMap.get(DcMotorEx.class, "turret");
+
 
         // Reset encoder
         turret.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         turret.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        turret.setDirection(DcMotorSimple.Direction.REVERSE);
+
         turret.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         limelight = hwMap.get(Limelight3A.class, "limelight");
         telemetry.addData("Version 1.0", "Turret Rotater!");
-        telemetry.addData("turretPos", pos);
-        telemetry.addData("turretPwr", power);
-        telemetry.addData("tx", limelight.getLatestResult().getTx());
         limelight.pipelineSwitch(0);
+        limelight.start();
 
 
         timer.reset();
     }
-    int pos = turret.getCurrentPosition();
 
     public void setP(double newKP) {
         kP = newKP;
@@ -118,13 +115,15 @@ public class TurretMechanism {
 
         power = Range.clip(pTerm + dTerm, -MAX_POWER, MAX_POWER);
 
+        int position = turret.getCurrentPosition();
 
-        // hard limits
-        if (pos >= rightLimit && power > 0) power = 0;
-        if (pos <= leftLimit  && power < 0) power = 0;
+        if (position > RIGHT_LIMIT && power > 0) {
+            power = 0;
+        }
 
-        turret.setPower(power);
-
+        if (position < LEFT_LIMIT && power < 0) {
+            power = 0;
+        }
 
         turret.setPower(power);
 
