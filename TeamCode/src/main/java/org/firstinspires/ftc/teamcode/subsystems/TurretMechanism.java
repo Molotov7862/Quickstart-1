73d1b591f4657cc98eb8c2695173ecdc2e9b4ca5
Molotov <irfaankhan2008@gmail.com>
2026-02-21 03:21:59 -0500

    Update TurretMechanism.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/TurretMechanism.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/TurretMechanism.java
@@ -5,6 +5,7 @@ import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
@@ -28,8 +29,8 @@ public class TurretMechanism {
     private final ElapsedTime timer = new ElapsedTime();
 
 
-    private final int limit = 600;
-
+    public static int leftLimit  = -600;
+    public static int rightLimit =  600;
 
 
     public void init(HardwareMap hwMap, Telemetry telemetry) {
@@ -39,17 +40,21 @@ public class TurretMechanism {
         // Reset encoder
         turret.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         turret.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
+        turret.setDirection(DcMotorSimple.Direction.REVERSE);
         turret.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         limelight = hwMap.get(Limelight3A.class, "limelight");
         telemetry.addData("Version 1.0", "Turret Rotater!");
+        telemetry.addData("turretPos", pos);
+        telemetry.addData("turretPwr", power);
+        telemetry.addData("tx", limelight.getLatestResult().getTx());
         limelight.pipelineSwitch(0);
         limelight.start();
 
 
         timer.reset();
     }
+    int pos = turret.getCurrentPosition();
 
     public void setP(double newKP) {
         kP = newKP;
@@ -113,16 +118,14 @@ public class TurretMechanism {
 
         power = Range.clip(pTerm + dTerm, -MAX_POWER, MAX_POWER);
 
-        int position = turret.getCurrentPosition();
 
-        if (position > limit && power > 0) {
-            power = 0;
-        }
+        // hard limits
+        if (pos >= rightLimit && power > 0) power = 0;
+        if (pos <= leftLimit  && power < 0) power = 0;
 
-        if (position < -limit && power < 0) {
-            power = 0;
-        }
-        telemetry.addData("TargetVel", targetVelocity);
+        turret.setPower(power);
+
+
         turret.setPower(power);
 
         lastError = error;
